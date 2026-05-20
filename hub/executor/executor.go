e8a344512b3371bf3e2117bf89daf2ec8acbe24b
Andy <2444909880@qq.com>
2026-05-20 11:09:53 +0800

    -

--- hub/executor/executor.go
+++ hub/executor/executor.go
@@ -333,11 +333,11 @@ func loadProvider[T P.Provider](providers map[string]T) {
 			switch pv.Type() {
 			case P.Proxy:
 				{
-					log.Errorln("initial proxy provider %s error: %v", name, err)
+					log.Warnln("initial proxy provider %s error: %v", name, err)
 				}
 			case P.Rule:
 				{
-					log.Errorln("initial rule provider %s error: %v", name, err)
+					log.Warnln("initial rule provider %s error: %v", name, err)
 				}
 			}
 		} else {
